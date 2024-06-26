b494e0b72bdc99735b0db8f80d7864e35695e98c
Imperial-Majesty <ujjwal.sinha@globallogic.com>
2024-06-26 10:39:36 +0000

    My second commit

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 const express = require('express');
 
+const ServerConfig = require('./config/ServerConfig');
+
+const app = express();
+
+app.listen(ServerConfig.PORT, () => {
+    console.log(`Server Listening to ${PORT}`);
+})
