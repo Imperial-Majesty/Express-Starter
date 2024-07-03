3a743659611525aeb2f95a76547efab7c087e1dc
Imperial-Majesty <ujjwal.sinha@globallogic.com>
2024-07-03 19:24:47 +0000

    Server undefined not resolved yet

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -2,6 +2,8 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+
+// Here we define the exporting the env variables that the project uses.
 module.exports = {
     PORT: process.env.PORT
 }
--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ const ServerConfig = require('./config/serverConfig');
 const app = express();
 
 app.listen(ServerConfig.PORT, () => {
-    console.log(`Server Listening to ${ServerConfig.PORT}`);
+    console.log(`Port started at ${ServerConfig.PORT}`);
 })
